6a77933f0a3d91b60dcb5f2dc6a743f55d801948
divyareddyvanipanta <divyareddyvanipanta@gmail.com>
2022-06-01 15:30:44 -0500

    categories data

--- frontend/src/app/category/category.component.ts
+++ frontend/src/app/category/category.component.ts
@@ -1,15 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
+export interface IMAGES{
+  image_id: Number,
+  image_path: String,
+  news_id: Number,
+}
+
+export interface NEWS {
+  category: String,
+  channel_id: String,
+  description: String,
+  edited_date: String,
+  hashtag: String,
+  headline: String,
+  images: IMAGES[],
+  news_id: Number,
+  posted_by: Number,
+  posted_date: String,
+}
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  dataFetched!: NEWS[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<NEWS[]>("http://127.0.0.1:5000/api/news").subscribe(data => {
+      console.log(data);
+      this.dataFetched = data;
+    })
   }
 
 }
